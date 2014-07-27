79036ed1242d25ed1191660d409f9251329a1353
Joe Wong <joe.wongchanglor@terem.com.au>
2014-07-28 01:45:50 +1000

    add tmux.conf

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ cp slate ~/.slate
 cp gemrc ~/.gemrc
 cp railsrc ~/.railsrc
 cp irbrc ~/.irbrc
+cp tmux.conf ~/.tmux.conf
 cp gitignore_global ~/.gitignore_global
 cp joe.zsh-theme ~/.oh-my-zsh/themes/joe.zsh-theme
--- update.sh
+++ update.sh
@@ -7,5 +7,6 @@ cp ~/.slate slate
 cp ~/.gemrc gemrc
 cp ~/.railsrc railsrc
 cp ~/.irbrc irbrc
+cp ~/.tmux.conf tmux.conf
 cp ~/.gitignore_global gitignore_global
 cp ~/.oh-my-zsh/themes/joe.zsh-theme joe.zsh-theme
