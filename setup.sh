1bb885b14bcdf47eeb0630f448a82ed4727224aa
Joe W <joe.wongchanglor@terem.com.au>
2015-09-14 11:40:32 +1000

    update setup script to copy over pryrc

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ cp slate ~/.slate
 cp gemrc ~/.gemrc
 cp railsrc ~/.railsrc
 cp irbrc ~/.irbrc
+cp pryrc ~/.pryrc
 cp tmux.conf ~/.tmux.conf
 cp gitignore_global ~/.gitignore_global
 cp joe.zsh-theme ~/.oh-my-zsh/themes/joe.zsh-theme
