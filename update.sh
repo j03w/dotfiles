4353f3c88d0dc32ffca6feaadef5ad927f874881
Joe W <joe.wongchanglor@terem.com.au>
2015-09-14 11:38:26 +1000

    update update script to include pry and redisconf

--- update.sh
+++ update.sh
@@ -10,3 +10,6 @@ cp ~/.irbrc irbrc
 cp ~/.tmux.conf tmux.conf
 cp ~/.gitignore_global gitignore_global
 cp ~/.oh-my-zsh/themes/joe.zsh-theme joe.zsh-theme
+cp ~/.pryrc pryrc
+cp ~/.irbrc irbrc
+cp ~/.redis/redis.conf redis.conf
